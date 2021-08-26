ecc9edfff9b6c2a465da9f993eb5059a27fc62aa
Arjun Loganathan <arjun.loganathan@hpe.com>
2021-08-26 12:55:45 -0700

    working axios with cors, dbsetup feature

--- backend/app.js
+++ backend/app.js
@@ -28,26 +28,8 @@ const pgConfig = {
 
 const pgPool = new Pool(pgConfig)
 
-// one client reserved for responding to endpoints
-//  -> the frontend should always have data to display
-pgPool.connect((err, queryHandler, release) => {
-    if (err) {
-        return console.error('Error acquiring client', err.stack)
-    }
-    const pgHandler = new postgresDbHandler(queryHandler)
+const pgHandler = new postgresDbHandler(pgPool)
 
-    app.get('/tabledata/:type', (req, res, next) => pgHandler.getAllData(req, res).catch(next))
-    app.post('/bledata', (req, res, next) => pgHandler.getBleDataAp(req, res).catch(next))
-    app.get('/chartdates', (req, res, next) => pgHandler.getChartDates(req, res).catch(next))
-    app.post('/chartaps', (req, res, next) => pgHandler.getChartAps(req, res).catch(next))
-    app.get('/dates', (req, res, next) => pgHandler.getDates(req, res).catch(next))
-    app.get('/approfile', (req, res, next) => pgHandler.getApProfile(req, res).catch(next))
-    app.get('/radioprofile', (req, res, next) => pgHandler.getRadioProfile(req, res).catch(next))
-    app.get('/usbprofile', (req, res, next) => pgHandler.getUsbProfile(req, res).catch(next))
-    app.post('/chart', (req, res, next) => pgHandler.getChartData(req, res).catch(next))
-    app.post('/tsdata', (req, res, next) => pgHandler.getTableData(req, res).catch(next))
-    app.post('/avgrssi', (req, res, next) => pgHandler.getAvgRssi(req, res).catch(next))
-})
 
 if (dbsetup) {
     const queryTimescale = `CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;`;
@@ -100,7 +82,7 @@ if (dbsetup) {
         console.log('usb db table created');
     });
 
-    const queryApTable = `CREATE TABLE IF NOT EXISTS aptable (device_id TEXT PRIMARY KEY, last_health_status TEXT NOT NULL, usb_devices TEXT[] NOT NULL, TEXT[] NOT NULL, ble_devices TEXT[] NOT NULL);`;
+    const queryApTable = `CREATE TABLE IF NOT EXISTS aptable (device_id TEXT PRIMARY KEY, last_health_status TEXT NOT NULL, usb_devices TEXT[] NOT NULL, radio_devices TEXT[] NOT NULL, ble_devices TEXT[] NOT NULL);`;
     pgPool.query(queryApTable, (err, res) => {
         if (err) {
             console.error(err);
@@ -110,7 +92,7 @@ if (dbsetup) {
         console.log('ap db table created');
     });
 
-    const queryBleTable = `CREATE TABLE bledevtable (device_identifier TEXT PRIMARY KEY, access_points TEXT[] NOT NULL);`;
+    const queryBleTable = `CREATE TABLE IF NOT EXISTS bledevtable (device_identifier TEXT PRIMARY KEY, access_points TEXT[] NOT NULL);`;
     pgPool.query(queryBleTable, (err, res) => {
         if (err) {
             console.error(err);
@@ -120,7 +102,6 @@ if (dbsetup) {
         console.log('ble db table created');
     });
 }
-
 
 app.use(cors())
 app.use(logger('dev'))
@@ -132,6 +113,18 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', function (req, res, next) {
     res.render('Welcome to backend', {title: 'Express'});
 });
+
+app.get('/tabledata/:type', (req, res, next) => pgHandler.getAllData(req, res).catch(next))
+app.post('/bledata', (req, res, next) => pgHandler.getBleDataAp(req, res).catch(next))
+app.get('/chartdates', (req, res, next) => pgHandler.getChartDates(req, res).catch(next))
+app.post('/chartaps', (req, res, next) => pgHandler.getChartAps(req, res).catch(next))
+app.get('/dates', (req, res, next) => pgHandler.getDates(req, res).catch(next))
+app.get('/approfile', (req, res, next) => pgHandler.getApProfile(req, res).catch(next))
+app.get('/radioprofile', (req, res, next) => pgHandler.getRadioProfile(req, res).catch(next))
+app.get('/usbprofile', (req, res, next) => pgHandler.getUsbProfile(req, res).catch(next))
+app.post('/chart', (req, res, next) => pgHandler.getChartData(req, res).catch(next))
+app.post('/tsdata', (req, res, next) => pgHandler.getTableData(req, res).catch(next))
+app.post('/avgrssi', (req, res, next) => pgHandler.getAvgRssi(req, res).catch(next))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
